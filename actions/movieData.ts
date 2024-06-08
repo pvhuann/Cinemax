830aed0e27e1e2f69f351da9339dc077ea963b4f
pvhuann <65993564+pvhuann@users.noreply.github.com>
2024-06-08 11:03:19 +0700

    fetch data

---
+++ actions/movieData.ts
@@ -0,0 +1,7 @@
+import { getApiResponse } from "@/lib/requests"
+
+export const fetchTrending= async () =>{
+    const data = await getApiResponse("/trending/movie/week") 
+    const trending = data.results
+    return trending
+}
